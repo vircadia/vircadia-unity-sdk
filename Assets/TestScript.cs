f08852d6d28ad6689e6fb89ebd69f853a1dea571
namark <nshan.nnnn@gmail.com>
2022-03-14 16:32:29 +0400

    File headers fixed to include license information.

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -1,3 +1,14 @@
+//
+//  Common.cs
+//  Scripts/Vircadia/Native
+//
+//  Created by Nshan G. on 03 Mar 2022.
+//  Copyright 2022 Vircadia contributors.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
