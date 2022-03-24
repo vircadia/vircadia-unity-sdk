de01fb4bfb4198fa380296695bd7abea85d6ba9b
namark <nshan.nnnn@gmail.com>
2022-03-24 04:29:24 +0400

    Basic Domain server class usage example.

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -13,12 +13,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class ApplicationInfo
+{
+
+    private string _name;
+    public string name
+    {
+        get { return _name ?? Application.productName; }
+        set { _name = value; }
+    }
+
+    private string _organization;
+    public string organization
+    {
+        get { return _organization ?? Application.companyName; }
+        set { _organization = value; }
+    }
+
+    public string domain;
+
+    private string _version;
+    public string version
+    {
+        get { return _version ?? Application.version; }
+        set { _version = value; }
+    }
+}
+
 public class TestScript : MonoBehaviour
 {
+    private Vircadia.DomainServer _domainServer;
+    private bool _connected = false;
+    private int _nodesSeen = 0;
+
+    public string location = "localhost";
+    public ApplicationInfo applicationInfo;
 
     void Start()
     {
         Debug.Log("Vircadia SDK native API version: " + Vircadia.Info.NativeVersion().full);
+
+        this._domainServer = new Vircadia.DomainServer(
+            appName: this.applicationInfo.name,
+            appOrganization: this.applicationInfo.organization,
+            appDomain: this.applicationInfo.domain,
+            appVersion: this.applicationInfo.version);
+
+        if (!string.IsNullOrEmpty(this.location))
+        {
+            this._domainServer.Connect(this.location);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (!this._connected)
+        {
+            Debug.Log("[vircadia-unity-example] Connecting");
+            if (this._domainServer.Status == Vircadia.DomainServerStatus.Connected)
+            {
+                Debug.Log("[vircadia-unity-example] Successfully connected to " + this.location);
+                this._connected = true;
+            }
+        }
+        else
+        {
+            var nodes = this._domainServer.Nodes;
+            if (nodes.Length != _nodesSeen)
+            {
+                Debug.Log("[vircadia-unity-example] Updated node list:");
+                foreach (var node in nodes)
+                {
+                    Debug.Log("[vircadia-unity-example] UUID: " + node.uuid.ToString());
+                }
+                _nodesSeen = nodes.Length;
+            }
+        }
     }
 
 }
